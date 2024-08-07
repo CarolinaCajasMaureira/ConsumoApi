376ffee58cd089a7168965ed1a08f699d2646733
Carolina Cajas <carolina.cajas.maureira@gmail.com>
2024-08-06 22:03:59 -0400

    first commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      © 2024 - Nostra Pizza - Todos los derechos reservados
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1><strong>Nostra Pizza</strong></h1>
+      <p><strong>¡Tan ricas que hablarás en Italiano!</strong></p>
+    </header>
+  );
+};
+
+export default Header;
